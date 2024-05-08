54e43e4a984054012a308cc752fc85493671ef6e
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-05-08 14:47:51 +0200

    configured logging using serilog

--- Esteti.WebApi/Program.cs
+++ Esteti.WebApi/Program.cs
@@ -1,10 +1,30 @@
+using Serilog;
+
 namespace Esteti.WebApi
 {
     public class Program
     {
+        public static string APP_NAME = "Esteti.WebApi";
+
         public static void Main(string[] args)
         {
+            //logging before the host is built
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.WithProperty("Application", APP_NAME)
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+
             var builder = WebApplication.CreateBuilder(args);
+
+            //logging after the host is built
+            builder.Host.UseSerilog((context, services, configuration) => configuration
+                .Enrich.WithProperty("Application", APP_NAME)
+                .Enrich.WithProperty("MachineName", Environment.MachineName)
+                .ReadFrom.Configuration(context.Configuration)
+                .ReadFrom.Services(services)
+                .Enrich.FromLogContext());
 
             // Add services to the container.
 
