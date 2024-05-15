2008c982ad8083a7302e2b1d8dd33bae57c3ec4f
Jakub Dzikowski <dzikowskijakub2001@gmail.com>
2024-05-15 15:37:29 +0200

    logout implemented

---
+++ Esteti.Application/Logic/User/LogoutCommand.cs
@@ -0,0 +1,51 @@
+﻿using Esteti.Application.Exceptions;
+using Esteti.Application.Interfaces;
+using Esteti.Application.Logic.Abstractions;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esteti.Application.Logic.User
+{
+    public static class LogoutCommand
+    {
+        public class Request : IRequest<Result>
+        {
+
+        }
+
+        public class Result
+        {
+
+        }
+
+        public class Handler : BaseCommandHandler, IRequestHandler<Request, Result>
+        {
+
+            public Handler(ICurrentAccountProvider currentAccountProvider, IApplicationDbContext applicationDbContext) : base(currentAccountProvider, applicationDbContext)
+            {
+            }
+
+            public async Task<Result> Handle(Request request, CancellationToken cancellationToken)
+            {
+                return new Result()
+                {
+                    //not implemented - maybe for adding logs that user logged out
+                };
+            }
+        }
+
+        public class Validator : AbstractValidator<Request>
+        {
+            public Validator()
+            {
+
+            }
+        }
+    }
+}
--- Esteti.WebApi/Controllers/UserController.cs
+++ Esteti.WebApi/Controllers/UserController.cs
@@ -44,6 +44,14 @@ namespace Esteti.WebApi.Controllers
             return Ok(new JwtToken() { AccessToken = token });
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            var logoutResult = await _mediator.Send(new LogoutCommand.Request());
+            DeleteTokenCookie();
+            return Ok(logoutResult);
+        }
+
         private void SetTokenCookie(string token)
         {
             var cookieOption = new CookieOptions
@@ -67,6 +75,14 @@ namespace Esteti.WebApi.Controllers
 
             Response.Cookies.Append(CookieSettings.CookieName, token, cookieOption);
 
+        }
+
+        private void DeleteTokenCookie()
+        {
+            Response.Cookies.Delete(CookieSettings.CookieName, new CookieOptions()
+            {
+                HttpOnly = true
+            });
         }
     }
 }
